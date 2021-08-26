1158119a05cc30de5ee1b2a37354c87286c493a0
Cameron White <cameronpwhite98@gmail.com>
2021-08-26 16:00:05 -0500

    created customer list to render

---
+++ src/components/Repairs.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from "react"
+import { CustomerList } from "./customers/CustomerList"
+
+export const Repairs = () => {
+    return (
+        <>
+        <h1>Honey Rae's Repair Shop</h1>
+        <CustomerList />
+        </>
+    )
+}
